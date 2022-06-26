e291db0d1a646b531d1feb6b74aaeedd574c54aa
grimcake <994459124@qq.com>
2022-06-26 22:45:53 +0800

    2-创建简单的映射器代理工厂

---
+++ src/main/java/cn/grimcake/mybatis/binding/MapperProxyFactory.java
@@ -0,0 +1,23 @@
+package cn.grimcake.mybatis.binding;
+
+import java.lang.reflect.Proxy;
+import java.util.Map;
+
+/**
+ * 映射器代理工厂
+ */
+public class MapperProxyFactory<T> {
+    /**
+     * 提供给哪一个类做代理操作
+     */
+    private final Class<T> mapperInterface;
+
+    public MapperProxyFactory(Class<T> mapperInterface) {
+        this.mapperInterface = mapperInterface;
+    }
+
+    public T newInstance(Map<String, String> sqlSession) {
+        MapperProxy<T> mapperProxy = new MapperProxy<>(sqlSession, mapperInterface);
+        return (T)Proxy.newProxyInstance(mapperInterface.getClassLoader(), new Class[]{mapperInterface}, mapperProxy);
+    }
+}
---
+++ src/test/java/cn/grimcake/mybatis/test/dao/IUserDao.java
@@ -0,0 +1,5 @@
+package cn.grimcake.mybatis.test.dao;
+
+public interface IUserDao {
+    String queryUserName(String name);
+}
